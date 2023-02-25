3eb1dcf3d81fdb4ea165bc932932d856d539b6fc
urusov2009@rambler.ru <urusov2009@rambler.ru>
2023-02-26 00:32:36 +0600

    завершение части beckend

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+
+def all_blogs(requests):
+    blogs = Blog.objects.all().order_by('-date')
+    return render(requests, "blog/all_blogs.html", {'blogs': blogs})
+
+
+def detail(requests, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(requests, 'blog/detail.html', {'blog': blog})
--- portfolio/my_portfolio/views.py
+++ portfolio/my_portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
+
+def home(requests):
+    projects = Project.objects.all()
+    return render(requests, "my_portfolio/home.html", {'projects': projects})
 # Create your views here.
